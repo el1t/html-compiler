ccecd406ba464fcf51be79eeb3b1be4b7e562d49
Ellis <el1t@ymail.com>
2015-04-06 21:56:30 -0400

    Combine inserted files
    And learn to python—this isn't javascript, you know. That means under_scores and no global variables.

--- html-compiler.py
+++ html-compiler.py
@@ -6,106 +6,30 @@ from os.path import exists, isfile
 from re import split
 from sys import argv
 
-extensions = {
-	'.css' : 'text/css',
-	'.jpg' : 'image/jpeg',
-	'.jpeg': 'image/jpeg',
-	'.js'  : 'text/javascript',
-	'.png' : 'image/png',
-}
-
-
-def css(output, path, attrs):
-	output.write('<style>')
-	for attr in attrs:
-		if attr[0] == 'href':
-			print('Inserting css file:', path + attr[1])
-			with open(path + attr[1], 'r') as read:
-				output.write(read.read())
-	output.write('</style>')
-
-
-def js(output, path, attrs):
-	read = None
-	output.write('<script')
-	for attr in attrs:
-		if attr[0] == 'src':
-			print('Inserting javascript file:', path + attr[1])
-			read = open(path + attr[1], 'r')
-		else:
-			output.write(' ' + attr[0] + '="' + ' '.join(attr[1:]) + '"')
-	output.write('>')
-	output.write(read.read())
-	read.close()
-	output.write('</script>')
-
-
-# def image(output, path, attrs):
-# 	output.write('<img')
-# 	for attr in attrs:
-# 		if attr[0] == 'src':
-# 			encode('src', attr[1], output, path)
-# 		else:
-# 			output.write(' ' + attr[0] + '="' + ' '.join(attr[1:]) + '"')
-# 	output.write(' />')
-
-
-def encode(path, output, local_path, attr=True):
-	if attr:
-		output.write('data:')
-	if not isfile(path) and path[0] != '/' and isfile(local_path + path):
-		# Prepend local path
-		path = local_path + path
-	if isfile(path):
-		print('Encoding local file:', path)
-		# Use local path
-		with open(path, 'r') as read:
-			output.write(extensions[path[path.rindex('.'):]] + ';base64,' + str(b64encode(read.read()), 'utf-8'))
-	else:
-		# Try remote path
-		print('Encoding remote file:', path)
-		output.write(extensions[path[path.rindex('.'):]] + ';base64,' + str(b64encode(urlopen(path).read()), 'utf-8'))
-
-
-def style(styles, output, local_path):
-	styles = split('url\(', styles)
-	output.write(styles[0])
-	if len(styles) > 1:
-		for style in styles[1:]:
-			output.write('url(data:')
-			encode(style[1:style.index(')') - 1], output, local_path, False)
-			output.write(style[style.index(')'):])
-
-
-handles = {
-	'link'  : css,
-	'script': js,
-	# 'img'   : image
-}
-
-handleAttribs = {
-	'src'  : encode,
-	'style': style
-}
-
 
 class Parser(HTMLParser):
 	def __init__(self, path, output):
 		super(Parser, self).__init__()
 		self.path = path
 		self.output = output
-		self.skipNextEndTag = False
+		self.skip_next_end_tag = False
+		self.files = {
+			'css': [],
+			'js' : []
+		}
 
 	def handle_starttag(self, tag, attrs):
-		if tag in handles:
-			handles[tag](self.output, self.path, attrs)
-			self.skipNextEndTag = True
+		if tag in self.handles:
+			for attr in attrs:
+				if attr[0] == self.handles[tag][1]:
+					# Queue up file for outputting later
+					self.files[self.handles[tag][0]].append(attr[1])
 		else:
 			self.output.write('<' + tag)
 			for attr in attrs:
 				self.output.write(' ' + attr[0] + '="')
-				if attr[0] in handleAttribs:
-					handleAttribs[attr[0]](attr[1], self.output, self.path)
+				if attr[0] in self.handleAttribs:
+					self.handleAttribs[attr[0]](self, attr[1])
 				else:
 					self.output.write(' '.join(attr[1:]))
 				self.output.write('"')
@@ -115,22 +39,92 @@ class Parser(HTMLParser):
 		self.output.write(data)
 
 	def handle_endtag(self, tag):
-		if not self.skipNextEndTag:
+		if not self.skip_next_end_tag:
+			# Output file queues at end of head and body
+			if tag.lower() == 'head':
+				self.output_css()
+				self.output_js()
+			elif tag.lower() == 'body':
+				self.output_js()
 			self.output.write('</' + tag + '>')
-		self.skipNextEndTag = False
+		else:
+			self.skip_next_end_tag = False
+
+	def handle_style(self, styles):
+		styles = split('url\(', styles)
+		self.output.write(styles[0])
+		if len(styles) > 1:
+			for style in styles[1:]:
+				self.output.write('url(data:')
+				print('Encoding file from', end=' ')
+				self.encode(self.resolve_path(style[1:style.index(')') - 1]), False)
+				self.output.write(style[style.index(')'):])
+
+	def resolve_path(self, path):
+		extension = self.extensions[path[path.rindex('.'):]]
+		if not isfile(path) and path[0] != '/' and isfile(self.path + path):
+			# Prepend local path
+			path = self.path + path
+		print(path)
+		if isfile(path):
+			# Use local path
+			with open(path, 'r') as read:
+				return read.read(), extension
+		else:
+			# Try remote path
+			return urlopen(path).read(), extension
+
+	def output_css(self):
+		self.output.write('<style>')
+		for file_path in self.files['css']:
+			print('Inserting css file from', end=' ')
+			self.output.write(self.resolve_path(file_path)[0])
+		self.output.write('</style>')
+		self.files['css'] = []
+
+	def output_js(self):
+		self.output.write('<script type="text/javascript">')
+		for file_path in self.files['js']:
+			print('Inserting javascript file from', end=' ')
+			self.output.write(self.resolve_path(file_path)[0])
+		self.output.write('</script>')
+		# Clear file queue for scripts in body of html
+		self.files['js'] = []
+
+	def encode(self, file, attr=True):
+		contents, extension = file
+		if attr:
+			self.output.write('data:')
+		self.output.write(extension + ';base64,' + str(b64encode(contents), 'utf-8'))
+
+	# Static vars
+	handleAttribs = {
+		'src'  : encode,
+		'style': handle_style
+	}
+	extensions = {
+		'.css' : 'text/css',
+		'.jpg' : 'image/jpeg',
+		'.jpeg': 'image/jpeg',
+		'.js'  : 'text/javascript',
+		'.png' : 'image/png',
+		}
+	handles = {
+		'link'  : ['css', 'href'],
+		'script': ['js',  'src']
+	}
 
 
 def main():
-	i = 0
-	for i, s in enumerate(argv[1][::-1]):
-		if s == '/':
-			break
+	file = argv[1]
+	index = file.rfind('/')
+	path = file[:index + 1] if index > -1 else './'
 	if not isfile(argv[1]):
 		print('File not found')
 		return
-	with open(argv[1][:i] + 'output.html', 'w') as output:
-		p = Parser(argv[1][:i], output)
-		p.feed(open(argv[1], 'r').read())
+	with open(path + 'output.html', 'w') as output:
+		p = Parser(path, output)
+		p.feed(open(file, 'r').read())
 
 if __name__ == '__main__':
 	main()
